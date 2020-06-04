a17f75ab8eac67aada9aea47f7fb8a721b19b0b2
IolandaPereira <iolandarodrigues270@gmail.com>
2020-06-04 15:44:29 -0300

    dia 2 finalizado

---
+++ nlw/scripts/create-point.js
@@ -0,0 +1,43 @@
+document
+    .querySelector("select[name=uf]") //selecionar o select que tem o name=uf
+   .addEventListener("change", getCities)
+      
+
+function populateUFs(){
+    const ufSelect = document.querySelector("select[name=uf]")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( (res) => {return res.json() }  )
+    .then (  states =>{
+
+        for (const state of states){
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+
+    } )
+}
+
+populateUFs();
+
+
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]") 
+    const stateInput = document.querySelector("input[name=state]") 
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.Value = event.target.options[indexOfSelectedState]
+    
+
+    const url=`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+    .then( (res) => {return res.json() }  )
+    .then ( cities =>{
+
+        for (const city of cities){
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+        citySelect.disabled=false
+    } )
+
+}
